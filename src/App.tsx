327493a727ffd3a0f69dfac5f60246ebf8e79897
racheliver <racheliver@gmail.com>
2023-11-28 13:38:15 +0200

    Introducing MovieReviewHub, your go-to platform for expressing your thoughts and opinions on the latest and greatest films! Dive into the immersive world of cinema and share your unique perspective with our user-friendly movie review application.

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/navbar";
+import { Auth } from "./pages/auth";
+
+function App() {
+  return (
+    <div>
+    <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/auth" element={<Auth/>} />
+          <Route path="/rated" element={<h1>Rated</h1>} />
+        </Routes>
+    </Router>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ src/pages/auth/index.tsx
@@ -0,0 +1,61 @@
+import { Grid, Header, Form, Segment, Button } from "semantic-ui-react";
+import { useMutation } from "@tanstack/react-query";
+import { mutationLogin } from "./mutation";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+export const Auth = () => {
+
+  const { data, mutate } = useMutation({
+    mutationKey: ["Login"],
+    mutationFn: mutationLogin,
+  });
+  
+  useEffect(() => {
+    const checkLocalStorage = async () => {
+      try {
+        if (localStorage.getItem("guest_session_id")) {
+          console.log(localStorage.getItem("guest_session_id"));
+        } else {
+          await mutate();
+
+          if (data && data.guest_session_id) {
+            const guestSessionId = data.guest_session_id;
+
+            console.log('index', guestSessionId);
+
+            localStorage.setItem("guest_session_id", guestSessionId);
+          }
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        // Handle any errors that occurred during mutation or local storage update
+      }
+    };
+
+    checkLocalStorage();
+  }, [data, mutate]);
+  const navigate = useNavigate();
+  const handlerLogin =  () => {
+
+    navigate("/rated");
+  
+  };
+
+  return (
+    <Grid textAlign="center" verticalAlign="middle" style={{ height: "100vh" }}>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="violet" textAlign="center">
+          Welcome! Login by registering as a guest below
+        </Header>
+        <Form size="large">
+          <Segment stacked>
+            <Button color="violet" size="large" fluid onClick={handlerLogin}>
+              Login
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
+  );
+
+};
